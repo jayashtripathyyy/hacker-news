5c9a4fd64f97d841466b89e4398dfe6440b9be8b
Jayash Tripathy <jayashtripathy372@gmail.com>
2025-02-14 19:29:48 +0530

    search functionality with immediate search on Enter

--- src/components/search/search-bar.tsx
+++ src/components/search/search-bar.tsx
@@ -1,23 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import { useStore } from '../../store/global';
 import useDebounce from '../../hooks/use-debounce';
+import { SearchIcon } from 'lucide-react';
 
 type Props = {}
 
 function SearchBar({ }: Props) {
   const { searchQuery, setSearchQuery } = useStore();
   const [inputValue, setInputValue] = useState(searchQuery);
-  const debouncedValue = useDebounce(inputValue, 800);
+  const [debouncedValue, clearDebounce] = useDebounce(inputValue, 800);
 
   useEffect(() => {
     setSearchQuery(debouncedValue);
   }, [debouncedValue]);
 
+
+  // if user pressed enter while typing
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      clearDebounce();
+      setSearchQuery(inputValue);
+    }
+  }
+
   return (
     <div className='border-b border-dashed border-border '>
       <div className='container_wrapper p-4'>
-        <div className='container_wrapper rounded-3xl border border-input bg-muted px-4 py-2 flex items-center gap-2'>
-          <input type='text' placeholder='Search' className='w-full bg-transparent outline-none' value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+        <div className='container_wrapper rounded-3xl border border-input bg-muted px-4 py-2 flex items-center gap-2 '>
+          <SearchIcon className='w-4 h-4' />
+          <input type='text' placeholder='Search' className='w-full bg-transparent outline-none  ' value={inputValue} onChange={(e) => setInputValue(e.target.value)} onKeyDown={handleKeyPress} />
         </div>
       </div>
     </div>
--- src/hooks/use-debounce.tsx
+++ src/hooks/use-debounce.tsx
@@ -1,20 +1,31 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 
-
-function useDebounce<T>(value: T, delay: number) { 
+function useDebounce<T>(value: T, delay: number): [T, () => void] { 
     const [debouncedValue, setDebouncedValue] = useState<T>(value);
+    const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null);
+
+    const clearDebounce = useCallback(() => {
+        if (timeoutId) {
+            window.clearTimeout(timeoutId);
+            setTimeoutId(null);
+        }
+    }, [timeoutId]);
 
     useEffect(() => {
-        const timeoutId = setTimeout(() => {
+        const id = setTimeout(() => {    
             setDebouncedValue(value);
         }, delay);
+        
+        setTimeoutId(id);
 
-        return () => clearTimeout(timeoutId);
+        return () => {
+            if (id) {
+                window.clearTimeout(id);
+            }
+        };
     }, [value, delay]);
 
-    return debouncedValue;
+    return [debouncedValue, clearDebounce];
 }
-
-
 
 export default useDebounce
