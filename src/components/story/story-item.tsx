2ec0cf414c1f3b881f0849ea4811e33a4a845300
Jayash Tripathy <jayashtripathy372@gmail.com>
2025-02-15 20:28:56 +0530

    improved some styling of story item

--- src/components/story/story-item.tsx
+++ src/components/story/story-item.tsx
@@ -54,21 +54,21 @@ const StoryItem = forwardRef<HTMLDivElement, StoryItemProps>(
             <div
                 ref={ref}
                 onClick={onClick}
-                className="py-4 pt-6  px-6 md:px-4 bg-card hover:bg-muted/40 cursor-pointer transition-colors border-b border-border "
+                className="py-4 md:pt-6 px-2 md:px-6 bg-card hover:bg-muted/40 cursor-pointer transition-colors border-b border-border h-full flex flex-col justify-between overflow-hidden "
             >
-                <div className='flex gap-3 overflow-hidden '>
+                <div className='flex flex-1 gap-3 overflow-hidden '>
                     <div className="w-10 h-10 aspect-square bg-secondary/50 rounded-xl flex justify-center items-center flex-col p-1 ">
                         <ChevronUp />
                         <div className='text-[.5rem] font-bold'>{story.points}</div>
                     </div>
-                    <div className='flex flex-col gap-2 flex-1 text-balance'>
-                        <h3 className="text-lg font-semibold">{story.title}</h3>
+                    <div className='flex flex-col  flex-1 overflow-hidden'>
+                        <h3 className="text-lg font-semibold line-clamp-2">{story.title}</h3>
                         {story.url && <a href={story.url} target='_blank' className='text-sm text-muted-foreground'>{story.url}</a>}
                     </div>
                 </div>
 
                 <div className='flex gap-2 justify-between items-center mt-4'>
-                    <div className="flex gap-2 text-sm text-muted-foreground md:flex-row flex-col">
+                    <div className="flex gap-2 text-sm md:text-xs text-muted-foreground md:flex-row flex-col">
                         <span> by {story.author}</span>
                         <span>
                             {formatDistanceToNow(new Date(story.created_at), { addSuffix: true })}
